e24668865c9cbc005c38b86009bbd3cde77f945c
Sergio Abraham González Sainz <s3rgio.sainz@gmail.com>
2022-04-05 23:54:05 -0500

    Initial commit

---
+++ src/data/menu.js
@@ -0,0 +1,62 @@
+export const menudata = {
+  breakfast: [
+    {
+      title: "buttermilk pancakes",
+      price: "$15",
+      description: "Pancakes are awesome, no description needed!",
+      image: "item-1"
+    },
+    {
+      title: "country delight",
+      price: "14",
+      description: "Eggs and stuff. Not as good as pancakes, but cheaper.",
+      image: "item-4"
+    },
+    {
+      title: "bacon overflow",
+      price: "15",
+      description: "Muffin with bacon. It's ok...",
+      image: "item-7"
+    }
+  ],
+  lunch: [
+    {
+      title: "Diner Double",
+      price: "15",
+      description: "Burger with fries.",
+      image: "item-2"
+    },
+    {
+      title: "egg attack",
+      price: "15",
+      description: "Burger, no fries.",
+      image: "item-5"
+    },
+    {
+      title: "american classic",
+      price: "15",
+      description: "Cheeseburger. Pepsi, no coke.",
+      image: "item-8"
+    }
+  ],
+  shakes: [
+    {
+      title: "Godzilla Milkshake",
+      price: "15",
+      description: "Made with real nuclear lizards!",
+      image: "item-3"
+    },
+    {
+      title: "Oreo Dream",
+      price: "15",
+      description: "Oreo, milk, blender. ",
+      image: "item-6"
+    },
+    {
+      title: "Quarantine Buddy",
+      price: "15",
+      description: "This makes no sense. Pancakes!",
+      image: "item-9"
+    }
+  ]
+};
